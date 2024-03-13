7912e3edf958c6a7a39cf6848e652ef407f118fa
Guilherme Pereira <guilospanck@protonmail.com>
2024-03-13 06:55:55 +0000

    Add missing price endpoints

--- src/prices/mod.rs
+++ src/prices/mod.rs
@@ -1,20 +1,22 @@
 pub mod types;
 use std::result;
 
-use reqwest::{self, Response};
+use reqwest;
 
-use self::types::{ConvertCurrencyJsonData, ConvertCurrencyParams, ConvertCurrencyReturn};
+use self::types::{
+  ConvertCurrencyJsonData, ConvertCurrencyParams, ConvertCurrencyReturn, GetTickerJsonData,
+  GetTickerParams, GetTickerReturn, GetTickersJsonData, GetTickersReturn,
+};
 
 const BASE_URL: &str = "http://prices.murrayrothbot.com";
 
-/// [`Event`] error
+/// [`Price`] error
 #[derive(thiserror::Error, Debug)]
 pub enum PriceError {
   #[error("Invalid URL params")]
   InvalidURLParams,
   #[error("Bad request")]
   BadRequest,
-
 }
 
 type Result<T> = result::Result<T, PriceError>;
@@ -23,17 +25,67 @@ type Result<T> = result::Result<T, PriceError>;
 pub async fn convert_currency(
   ConvertCurrencyParams { currency, value }: ConvertCurrencyParams,
 ) -> Result<ConvertCurrencyReturn> {
-
   let url = format!("{}/convert", BASE_URL);
-  let params = [("currency", currency.to_string()), ("value", value.to_string())];
+  let params = [
+    ("currency", currency.to_string()),
+    ("value", value.to_string()),
+  ];
 
   let url = match reqwest::Url::parse_with_params(&url, &params) {
     Ok(url) => url,
     Err(_) => return Err(PriceError::InvalidURLParams),
   };
 
-  let resp = match reqwest::get(url).await {
+  let client = reqwest::Client::new()
+    .get(url)
+    .header("Accept", "application/json");
+
+  let resp = match client.send().await {
     Ok(resp) => resp.json::<ConvertCurrencyJsonData>().await.unwrap(),
+    Err(_) => return Err(PriceError::BadRequest),
+  };
+
+  Ok(resp.data)
+}
+
+#[tokio::main]
+pub async fn get_ticker(GetTickerParams { symbol }: GetTickerParams) -> Result<GetTickerReturn> {
+  let url = format!("{}/ticker", BASE_URL);
+  let params = [("symbol", symbol.to_string())];
+
+  let url = match reqwest::Url::parse_with_params(&url, &params) {
+    Ok(url) => url,
+    Err(_) => return Err(PriceError::InvalidURLParams),
+  };
+
+  let client = reqwest::Client::new()
+    .get(url)
+    .header("Accept", "application/json");
+
+  let resp = match client.send().await {
+    Ok(resp) => resp.json::<GetTickerJsonData>().await.unwrap(),
+    Err(_) => return Err(PriceError::BadRequest),
+  };
+
+  Ok(resp.data)
+}
+
+#[tokio::main]
+pub async fn get_tickers(GetTickerParams { symbol }: GetTickerParams) -> Result<GetTickersReturn> {
+  let url = format!("{}/tickers", BASE_URL);
+  let params = [("symbol", symbol.to_string())];
+
+  let url = match reqwest::Url::parse_with_params(&url, &params) {
+    Ok(url) => url,
+    Err(_) => return Err(PriceError::InvalidURLParams),
+  };
+
+  let client = reqwest::Client::new()
+    .get(url)
+    .header("Accept", "application/json");
+
+  let resp = match client.send().await {
+    Ok(resp) => resp.json::<GetTickersJsonData>().await.unwrap(),
     Err(_) => return Err(PriceError::BadRequest),
   };
 
--- src/prices/types.rs
+++ src/prices/types.rs
@@ -2,9 +2,7 @@
 
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Debug, PartialEq, strum::Display)]
-#[strum(ascii_case_insensitive)]
 pub enum Currency {
   BTC,
   BRL,
@@ -28,4 +26,46 @@ pub struct ConvertCurrencyReturn {
 #[derive(Deserialize, Serialize)]
 pub(crate) struct ConvertCurrencyJsonData {
   pub data: ConvertCurrencyReturn
+}
+
+#[derive(Debug, PartialEq, strum::Display)]
+pub enum Symbol {
+  BTCUSD,
+  BTCBRL,
+  BTCUSDT,
+}
+
+pub struct GetTickerParams {
+  pub symbol: Symbol,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetTickerReturn {
+  pub price: String,
+  pub change24h: String,
+  pub source: String,
+  pub symbol: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub(crate) struct GetTickerJsonData {
+  pub data: GetTickerReturn
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Tickers {
+  pub price: String,
+  pub change24h: Option<String>,
+  pub source: String,
+  pub symbol: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetTickersReturn {
+  pub tickers: Vec<Tickers>
+}
+
+#[derive(Deserialize, Serialize)]
+pub(crate) struct GetTickersJsonData {
+  pub data: GetTickersReturn
 }
