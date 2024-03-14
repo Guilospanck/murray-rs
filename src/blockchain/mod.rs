4ca21d5cceaef0b5f160e934f2a0ccd126b93dd8
Guilherme Pereira <guilospanck@protonmail.com>
2024-03-14 08:11:47 +0000

    Start implementing blockchain

---
+++ src/blockchain/mod.rs
@@ -0,0 +1,97 @@
+pub mod types;
+use std::result;
+
+use lazy_static;
+use reqwest::{self, Client};
+use std::sync::{Arc, RwLock};
+
+use self::types::{GetBlockParams, GetBlockResponse, GetBlockResponseJsonData};
+
+
+const BASE_URL: &str = "http://blockchain.murrayrothbot.com";
+
+lazy_static::lazy_static! {
+  static ref BLOCKCHAIN: Arc<RwLock<Blockchain>> = Arc::new(RwLock::new(Blockchain::new(BASE_URL.to_string())));
+}
+
+/// [`Price`] error
+#[derive(thiserror::Error, Debug)]
+pub enum PriceError {
+  #[error("Invalid URL params: `{0}`")]
+  InvalidURLParams(String),
+  #[error("Bad request: `{0}`")]
+  BadRequest(String),
+  #[error("JSON parse error: `{0}`")]
+  JSONParseError(String),
+}
+
+type Result<T> = result::Result<T, PriceError>;
+
+struct Blockchain {
+  base_url: String,
+  client: Client,
+}
+
+impl Blockchain {
+  pub fn new(url: String) -> Self {
+    let client = reqwest::Client::new();
+    Self {
+      base_url: url,
+      client,
+    }
+  }
+
+  pub fn set_base_url(&mut self, base_url: String) {
+    self.base_url = base_url;
+  }
+
+  #[tokio::main]
+  pub async fn get_block(
+    &self,
+    GetBlockParams { hash, height }: GetBlockParams,
+  ) -> Result<GetBlockResponse> {
+    let url = format!("{}/block", self.base_url);
+
+    let mut params = vec![];
+
+    match hash {
+        Some(s) => params.push(("hash", s)),
+        None => ()
+    };
+
+    match height {
+        Some(s) => params.push(("height", s.to_string())),
+        None => ()
+    };
+
+    let url = match reqwest::Url::parse_with_params(&url, &params) {
+      Ok(url) => url,
+      Err(e) => return Err(PriceError::InvalidURLParams(e.to_string())),
+    };
+
+    let client = self.client.get(url).header("Accept", "application/json");
+
+    let resp = match client.send().await {
+      Ok(resp) => match resp.json::<GetBlockResponseJsonData>().await {
+        Ok(r) => r.data,
+        Err(e) => return Err(PriceError::JSONParseError(e.to_string()))
+      },
+      Err(e) => return Err(PriceError::BadRequest(e.to_string())),
+    };
+
+    Ok(resp)
+  }
+
+ 
+}
+
+pub fn set_base_url(url: String) {
+  let mut blockchain = BLOCKCHAIN.write().unwrap();
+  blockchain.set_base_url(url);
+}
+
+pub fn get_block(params: GetBlockParams) -> Result<GetBlockResponse> {
+  let blockchain = BLOCKCHAIN.read().unwrap();
+  blockchain.get_block(params)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use murray_rs::prices::{self, types::{ConvertCurrencyParams, Currency, GetTickerParams, Symbol}};
-
-
+use murray_rs::{
+  blockchain::{self, types::GetBlockParams},
+  // prices::{
+  //   self,
+  //   types::{ConvertCurrencyParams, Currency, GetTickerParams, Symbol},
+  // },
+};
 
 fn main() {
-  /** Prices */
-  prices::set_base_url("http://localhost:3000".to_string());
-  let a = prices::convert_currency(ConvertCurrencyParams { currency: Currency::BRL, value: 100 });
+  /** Prices
+   * 
+    prices::set_base_url("http://localhost:3000".to_string());
+    let a = prices::convert_currency(ConvertCurrencyParams { currency: Currency::BRL, value: 100 });
+    println!("{:?}", a.unwrap());
+    let b = prices::get_ticker(GetTickerParams{symbol: Symbol::BTCUSD});
+    println!("{:?}", b.unwrap());
+    let c = prices::get_tickers(GetTickerParams{symbol: Symbol::BTCUSD});
+    println!("{:?}", c.unwrap());
+  */
+
+  /** Blockchain */
+  blockchain::set_base_url("http://localhost:3000".to_string());
+  let a = blockchain::get_block(GetBlockParams {
+    hash: None,
+    height: None,
+  });
   println!("{:?}", a.unwrap());
-  let b = prices::get_ticker(GetTickerParams{symbol: Symbol::BTCUSD});
-  println!("{:?}", b.unwrap());
-  let c = prices::get_tickers(GetTickerParams{symbol: Symbol::BTCUSD});
-  println!("{:?}", c.unwrap());
 }
